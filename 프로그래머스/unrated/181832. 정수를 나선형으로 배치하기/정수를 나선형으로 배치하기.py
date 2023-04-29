af1fc74de7445384f9b08ac6e550f6a0793d70a0
ChanHeeEEEEE <129827717+ChanHeeEEEEE@users.noreply.github.com>
2023-04-29 18:35:08 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.29 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,40 +1,17 @@
+dx=(0,1,0,-1)
+dy=(1,0,-1,0)
 def solution(n):
-    ans=[[0 for _ in range(n)] for _ in range(n)]
-    step=0
-    ans[0][0]=1
-    i=0
-    j=0
-    ii=0
-    jj=0
-    for k in range(0,n*n-1):
-        if step ==0:
-            jj =j+1
-            if jj>=n or ans[i][jj]!=0 : 
-                step+=1
-        if step == 1:
-            ii= i+1
-            if ii>=n or ans[ii][j]!=0:
-                step+=1
-            else:
-                i=ii
-        if step ==2:
-            jj= j-1
-            if jj<0 or ans[i][jj]!=0:
-                step+=1               
-            else:
-                j=jj
-        if step ==3:
-            ii =i-1
-            if ii<0 or ans[ii][j]!=0:
-                step-=3
-            else:
-                i=ii
-        if step ==0:
-            jj = j+1
-            if jj>=n or ans[i][jj]!=0 : 
-                step+=1
-            else:
-                j=jj
-        ans[i][j] = k+2
-        print(ans[i][j])
-    return ans
+    pro=[[0]*n for i in range(n)]
+    x,y=0,0
+    loc=2
+    d=0
+    pro[0][0]=1
+    while loc<=n*n:
+        nx,ny=x+dx[d],y+dy[d]
+        if not(0<=nx<n and 0<=ny<n) or pro[nx][ny] !=0:
+            d=(d+1)%4
+            continue
+        x,y=nx,ny
+        pro[x][y]=loc
+        loc +=1
+    return pro
