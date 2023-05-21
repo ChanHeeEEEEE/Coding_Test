3b97455a93f89b21755c71515dbfb2f72ad61d40
ChanHeeEEEEE <129827717+ChanHeeEEEEE@users.noreply.github.com>
2023-05-21 21:56:16 +0900

    [level 0] Title: 7의 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
@@ -0,0 +1,10 @@
+def solution(array):
+    # a=list(map(str,array))
+    # a="".join(a)
+    # a=list(a)
+    # c=0
+    # for i in a:
+    #     if i=='7':
+    #         c+=1
+    # return c
+    return str(array).count('7')
