1253b7c99219d2beff8da9313608175f86211ac4
ChanHeeEEEEE <129827717+ChanHeeEEEEE@users.noreply.github.com>
2023-08-01 21:18:38 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT 
+    O.ANIMAL_ID, O.NAME
+    # *
+from ANIMAL_INS I right join ANIMAL_OUTS O 
+on I.ANIMAL_ID = O.ANIMAL_ID
+where I.ANIMAL_ID is null
+order by O.ANIMAL_ID
+
+
+
+
