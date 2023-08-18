af3d666d71ee9a6e2eb19d4b1609c4273b4639d3
ChanHeeEEEEE <129827717+ChanHeeEEEEE@users.noreply.github.com>
2023-08-18 13:38:47 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,7 +1,33 @@
-SELECT CART_ID FROM (SELECT CART_ID,COUNT(CART_ID) CNT FROM
-(SELECT 
-   Distinct CART_ID , NAME from CART_PRODUCTS
-where NAME in ('Milk','Yogurt'))A
-group by CART_ID
-HAVING CNT >1)B
-ORDER BY CART_ID
+# select cart_id
+# from CART_PRODUCTS
+# group by cart_id
+# having group_concat(name) like '%Milk%' and group_concat(name) like '%Yogurt%'
+# order by 1
+
+SELECT DISTINCT A.CART_ID
+FROM CART_PRODUCTS A 
+INNER JOIN CART_PRODUCTS B ON A.CART_ID = B.CART_ID
+WHERE A.NAME LIKE 'Yogurt' AND B.NAME LIKE 'Milk'
+ORDER BY A.CART_ID
+
+
+# select cart_id
+# from (select cart_id, name
+#      from cart_products
+#      where name in ('Yogurt', 'Milk')
+#      group by cart_id,name)
+#      a
+# group by cart_id
+# having count(cart_id)>=2
+# order by cart_id;
+
+
+# select cart_id
+# from (
+#     select distinct cart_id
+#         , NAME
+#     from CART_PRODUCTS
+#     where name in ('Milk','Yogurt')) tbl
+# group by 1
+# having count(cart_id) >= 2
+# order by 1
