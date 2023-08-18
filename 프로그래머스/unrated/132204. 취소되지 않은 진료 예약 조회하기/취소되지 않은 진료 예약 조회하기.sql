aee1e749aec692c5efe315129e19f3027f225439
ChanHeeEEEEE <129827717+ChanHeeEEEEE@users.noreply.github.com>
2023-08-18 11:28:47 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,29 +1,20 @@
-# SELECT P.APNT_NO, P.PT_NAME, P.PT_NO, D.MCDP_CD,D.DR_NAME, P.APNT_YMD,P.APNT_CNCL_YMD
-#     from DOCTOR D join 
-#     (SELECT A.APNT_NO, A.PT_NO, PA.PT_NAME,A.MDDR_ID, A.APNT_YMD,A.APNT_CNCL_YMD
-#      from PATIENT PA join APPOINTMENT A 
-#     on PA.PT_NO = A.PT_NO
-#         where A.APNT_CNCL_YMD is null or A.APNT_CNCL_YMD >=' 2022-4-13'
-#         )P 
-#         on D.DR_ID=P.MDDR_ID
-# where D.MCDP_CD = "CS"
-# order by P.APNT_YMD
-
-
-SELECT P.APNT_NO, P.PT_NAME, P.PT_NO, D.MCDP_CD,D.DR_NAME, P.APNT_YMD
-    # ,P.APNT_CNCL_YMD
-    # , p.k
-    from DOCTOR D join 
-    (SELECT A.APNT_NO, A.PT_NO, PA.PT_NAME,A.MDDR_ID, A.APNT_YMD
-     # , DATE_FORMAT(A.APNT_YMD,'%Y-%m-%d') k
-     ,A.APNT_CNCL_YMD
-     from PATIENT PA join APPOINTMENT A 
-    on PA.PT_NO = A.PT_NO
-        where 
-     A.APNT_CNCL_YMD is null 
-     and 
-     DATE_FORMAT(A.APNT_YMD,'%Y-%m-%d') = '2022-04-13'
-        )P 
-        on D.DR_ID=P.MDDR_ID
+SELECT A.APNT_NO, P.PT_NAME, A.PT_NO, D.MCDP_CD, D.DR_NAME, A.APNT_YMD
+from APPOINTMENT A
+join PATIENT P ON A.PT_NO = P.PT_NO 
+JOIN DOCTOR  D ON A.MDDR_ID = D.DR_ID
 where D.MCDP_CD = "CS"
-order by P.APNT_YMD
+    and Date_format(A.APNT_YMD ,"%Y-%m-%d") = '2022-04-13'
+    and A.APNT_CNCL_YMD is null
+ORDER BY APNT_YMD ASC
+
+
+
+
+# SELECT A.APNT_NO, P.PT_NAME, A.PT_NO, D.MCDP_CD, D.DR_NAME, A.APNT_YMD
+# FROM APPOINTMENT A
+# INNER JOIN PATIENT P ON A.PT_NO = P.PT_NO 
+# INNER JOIN DOCTOR  D ON A.MDDR_ID = D.DR_ID
+# WHERE DATE_FORMAT(A.APNT_YMD,'%Y-%m-%d') = '2022-04-13' 
+# AND A.APNT_CNCL_YN = 'N'
+# AND A.MCDP_CD = 'CS'
+# ORDER BY APNT_YMD ASC
